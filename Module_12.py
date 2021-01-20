fdb59428c48ad5138130031bb7ea54fb3e5bffca
Alena Sidorova <sidsidsid1111@gmail.com>
2021-01-20 15:39:40 +0300

    Project Module_12 added

---
+++ Module_12.py
@@ -0,0 +1,7 @@
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+money = int(input("Введите сумму вклада: "))
+deposit = map(round, [money * (per_cent['ТКБ'])/100,
+                      money * (per_cent['СКБ'])/100,
+                      money * (per_cent['ВТБ'])/100,
+                      money * (per_cent['СБЕР'])/100])
+print('Максимальная сумма, которую вы можете заработать - ', max(deposit))
